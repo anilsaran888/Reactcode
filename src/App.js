b593386a7b54fe2f65f2b6e2974f25efc9cac530
Anil Kumar <anilsaran003@gmail.com>
2023-05-29 12:23:40 +0530

    Reactcode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import {useSelector} from'react-redux'
+import {useDispatch} from'react-redux'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const curState = useSelector((state)=> state.number);
+const dispatch = useDispatch();
+
+  return(
+    <>
+  <h1>React Redux Learning</h1>
+  <div style={ {width: '100%',display:'flex', alignItems:'center'}}>
+<h1>{curState}</h1>
+<div>
+<button onClick={()=> dispatch(Inc(10))} >Inc</button>
+<button onClick={()=> dispatch(Dec(5))}>Dec</button>
+
+</div>
+  </div>
+
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
+import { createSlice } from "@reduxjs/toolkit"
+export const incedecSlice = createSlice({
+name: 'incdec',
+initialState: 0,
+reducers:{
+  Inc: (state, action)=>{
+    return state += action.payload
+  },
+  Dec:(state, action)=>{
+    return state -= action.payload
+  }
+}
+
+})
+
+export const {Inc, Dec} = incedecSlice.actions;
+export default incedecSlice.reducer;
